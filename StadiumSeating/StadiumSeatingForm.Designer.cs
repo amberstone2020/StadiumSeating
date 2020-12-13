fab33b7367baff785e8f16483054cfddf4c985ec
Amber Stone <51244364+amberstone2020@users.noreply.github.com>
2020-12-12 23:37:02 -0500

    Add files via upload

---
+++ StadiumSeating/StadiumSeatingForm.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace StadiumSeating
+{
+    partial class StadiumSeatingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxC1 = new System.Windows.Forms.TextBox();
+            this.textBoxB1 = new System.Windows.Forms.TextBox();
+            this.textBoxA1 = new System.Windows.Forms.TextBox();
+            this.labelC1 = new System.Windows.Forms.Label();
+            this.labelB1 = new System.Windows.Forms.Label();
+            this.labelA1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxTotal = new System.Windows.Forms.TextBox();
+            this.textBoxC2 = new System.Windows.Forms.TextBox();
+            this.textBoxB2 = new System.Windows.Forms.TextBox();
+            this.textBoxA2 = new System.Windows.Forms.TextBox();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelC2 = new System.Windows.Forms.Label();
+            this.labelB2 = new System.Windows.Forms.Label();
+            this.labelA2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxC1);
+            this.groupBox1.Controls.Add(this.textBoxB1);
+            this.groupBox1.Controls.Add(this.textBoxA1);
+            this.groupBox1.Controls.Add(this.labelC1);
+            this.groupBox1.Controls.Add(this.labelB1);
+            this.groupBox1.Controls.Add(this.labelA1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 272);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tickets Sold";
+            // 
+            // textBoxC1
+            // 
+            this.textBoxC1.Location = new System.Drawing.Point(163, 196);
+            this.textBoxC1.Name = "textBoxC1";
+            this.textBoxC1.Size = new System.Drawing.Size(140, 26);
+            this.textBoxC1.TabIndex = 2;
+            // 
+            // textBoxB1
+            // 
+            this.textBoxB1.Location = new System.Drawing.Point(163, 147);
+            this.textBoxB1.Name = "textBoxB1";
+            this.textBoxB1.Size = new System.Drawing.Size(140, 26);
+            this.textBoxB1.TabIndex = 1;
+            // 
+            // textBoxA1
+            // 
+            this.textBoxA1.Location = new System.Drawing.Point(163, 96);
+            this.textBoxA1.Name = "textBoxA1";
+            this.textBoxA1.Size = new System.Drawing.Size(140, 26);
+            this.textBoxA1.TabIndex = 0;
+            // 
+            // labelC1
+            // 
+            this.labelC1.AutoSize = true;
+            this.labelC1.Location = new System.Drawing.Point(57, 196);
+            this.labelC1.Name = "labelC1";
+            this.labelC1.Size = new System.Drawing.Size(67, 20);
+            this.labelC1.TabIndex = 4;
+            this.labelC1.Text = "Class C:";
+            // 
+            // labelB1
+            // 
+            this.labelB1.AutoSize = true;
+            this.labelB1.Location = new System.Drawing.Point(57, 147);
+            this.labelB1.Name = "labelB1";
+            this.labelB1.Size = new System.Drawing.Size(67, 20);
+            this.labelB1.TabIndex = 3;
+            this.labelB1.Text = "Class B:";
+            // 
+            // labelA1
+            // 
+            this.labelA1.AutoSize = true;
+            this.labelA1.Location = new System.Drawing.Point(57, 96);
+            this.labelA1.Name = "labelA1";
+            this.labelA1.Size = new System.Drawing.Size(67, 20);
+            this.labelA1.TabIndex = 0;
+            this.labelA1.Text = "Class A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "for each class of seats.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the number of tickets sold ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxTotal);
+            this.groupBox2.Controls.Add(this.textBoxC2);
+            this.groupBox2.Controls.Add(this.textBoxB2);
+            this.groupBox2.Controls.Add(this.textBoxA2);
+            this.groupBox2.Controls.Add(this.labelTotal);
+            this.groupBox2.Controls.Add(this.labelC2);
+            this.groupBox2.Controls.Add(this.labelB2);
+            this.groupBox2.Controls.Add(this.labelA2);
+            this.groupBox2.Location = new System.Drawing.Point(444, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 272);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Revenue Generated";
+            // 
+            // textBoxTotal
+            // 
+            this.textBoxTotal.Location = new System.Drawing.Point(128, 195);
+            this.textBoxTotal.Name = "textBoxTotal";
+            this.textBoxTotal.ReadOnly = true;
+            this.textBoxTotal.Size = new System.Drawing.Size(150, 26);
+            this.textBoxTotal.TabIndex = 7;
+            this.textBoxTotal.TabStop = false;
+            // 
+            // textBoxC2
+            // 
+            this.textBoxC2.Location = new System.Drawing.Point(128, 147);
+            this.textBoxC2.Name = "textBoxC2";
+            this.textBoxC2.ReadOnly = true;
+            this.textBoxC2.Size = new System.Drawing.Size(150, 26);
+            this.textBoxC2.TabIndex = 6;
+            this.textBoxC2.TabStop = false;
+            // 
+            // textBoxB2
+            // 
+            this.textBoxB2.Location = new System.Drawing.Point(128, 95);
+            this.textBoxB2.Name = "textBoxB2";
+            this.textBoxB2.ReadOnly = true;
+            this.textBoxB2.Size = new System.Drawing.Size(150, 26);
+            this.textBoxB2.TabIndex = 5;
+            this.textBoxB2.TabStop = false;
+            // 
+            // textBoxA2
+            // 
+            this.textBoxA2.Location = new System.Drawing.Point(128, 44);
+            this.textBoxA2.Name = "textBoxA2";
+            this.textBoxA2.ReadOnly = true;
+            this.textBoxA2.Size = new System.Drawing.Size(150, 26);
+            this.textBoxA2.TabIndex = 4;
+            this.textBoxA2.TabStop = false;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(44, 202);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(48, 20);
+            this.labelTotal.TabIndex = 3;
+            this.labelTotal.Text = "Total:";
+            // 
+            // labelC2
+            // 
+            this.labelC2.AutoSize = true;
+            this.labelC2.Location = new System.Drawing.Point(44, 153);
+            this.labelC2.Name = "labelC2";
+            this.labelC2.Size = new System.Drawing.Size(67, 20);
+            this.labelC2.TabIndex = 2;
+            this.labelC2.Text = "Class C:";
+            // 
+            // labelB2
+            // 
+            this.labelB2.AutoSize = true;
+            this.labelB2.Location = new System.Drawing.Point(44, 101);
+            this.labelB2.Name = "labelB2";
+            this.labelB2.Size = new System.Drawing.Size(67, 20);
+            this.labelB2.TabIndex = 1;
+            this.labelB2.Text = "Class B:";
+            // 
+            // labelA2
+            // 
+            this.labelA2.AutoSize = true;
+            this.labelA2.Location = new System.Drawing.Point(44, 51);
+            this.labelA2.Name = "labelA2";
+            this.labelA2.Size = new System.Drawing.Size(67, 20);
+            this.labelA2.TabIndex = 0;
+            this.labelA2.Text = "Class A:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(212, 315);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(117, 56);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate Revenue";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(361, 315);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 56);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(492, 315);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(106, 56);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // StadiumSeatingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 383);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StadiumSeatingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stadium Seating ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxC1;
+        private System.Windows.Forms.TextBox textBoxB1;
+        private System.Windows.Forms.TextBox textBoxA1;
+        private System.Windows.Forms.Label labelC1;
+        private System.Windows.Forms.Label labelB1;
+        private System.Windows.Forms.Label labelA1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxTotal;
+        private System.Windows.Forms.TextBox textBoxC2;
+        private System.Windows.Forms.TextBox textBoxB2;
+        private System.Windows.Forms.TextBox textBoxA2;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelC2;
+        private System.Windows.Forms.Label labelB2;
+        private System.Windows.Forms.Label labelA2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ StadiumSeating/StadiumSeatingForm.cs
@@ -0,0 +1,140 @@
+﻿// Amber Holcomb-Stone
+// CITC 2311: Lab 1 Part B
+// Description: Create an application that allows the user to enter the number
+// of tickets sold for each class. The application should be able to display 
+// the amount of income generated from each class of ticket sales and the total revenue generated.
+// This application should use data validation to make sure that the data given is in an acceptable format.
+// Code partially adapted from: https://www.homeworklib.com/questions/315642/create-stadium-seating-visual-basic
+// and Murach's Book Applications Files
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StadiumSeating
+{
+    public partial class StadiumSeatingForm : Form
+    {
+        public StadiumSeatingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            Int32 ClassAPurchased;
+            Int32 ClassBPurchased;
+            Int32 ClassCPurchased;
+            int revenueTotal;
+
+            //Checks for Valid Data and Completes Exception Handling if needed
+            try
+            {
+                if (IsValidData())
+                {
+
+                    //Read tickets sold from corresponding textbox
+                    ClassAPurchased = Int32.Parse(textBoxA1.Text);
+                    ClassBPurchased = Int32.Parse(textBoxB1.Text);
+                    ClassCPurchased = Int32.Parse(textBoxC1.Text);
+
+                    //Calculate revenue for each ticket class
+                    ClassAPurchased = ClassAPurchased * 15;
+                    ClassBPurchased = ClassBPurchased * 12;
+                    ClassCPurchased = ClassCPurchased * 9;
+
+                    //calculate total revenue
+                    revenueTotal = ClassAPurchased + ClassBPurchased + ClassCPurchased;
+
+                    //Display calculations to corresponding revenue textbox with formatting
+                    textBoxA2.Text = ClassAPurchased.ToString("c");
+                    textBoxB2.Text = ClassBPurchased.ToString("c");
+                    textBoxC2.Text = ClassCPurchased.ToString("c");
+                    textBoxTotal.Text = revenueTotal.ToString("c");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" +
+                    ex.GetType().ToString() + "\n" +
+                    ex.StackTrace, "Exception");
+            }
+        }
+            //Checks if user input has been entered and if input is correct data type
+            public bool IsValidData()
+        {
+            return
+                // Validate the Class A text box
+                IsPresent(textBoxA1, "Class A") &&
+                IsInteger(textBoxA1, "Class A") &&
+
+
+                // Validate the Class B text box
+                IsPresent(textBoxB1, "Class B") &&
+                IsInteger(textBoxB1, "Class B") &&
+
+
+                // Validate the Class C text box
+                IsPresent(textBoxC1, "Class C") &&
+                IsInteger(textBoxC1, "Class C");
+                
+        }
+
+        // Checks if user has entered input and returns error message if no input has been entered
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        // Checks input for integer and returns error message if input is not an integer
+        public bool IsInteger(TextBox textBox, string name)
+        {
+            Int32 number = 0;
+            if (Int32.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be an integer value.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        // 'Clear' Button code
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clear the form
+            textBoxA1.Text = "";
+            textBoxB1.Text = "";
+            textBoxC1.Text = "";
+            textBoxA2.Text = "";
+            textBoxB2.Text = "";
+            textBoxC2.Text = "";
+            textBoxTotal.Text = "";
+        }
+
+        // Close the form
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
+
